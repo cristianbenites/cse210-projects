cb405a207192cd5e31cf6ec6ef2db60ae71215a7
cristianbenites <cristian.jbenites@gmail.com>
2024-09-19 00:12:11 -0400

    finish prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int number = new Random().Next(1, 100);
+        int numberOfGuesses = 0;
+
+        bool found = false;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            int guess = int.Parse(userInput);
+            numberOfGuesses++;
+
+            if(guess == number)
+            {
+                found = true;
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"Number of guesses: {numberOfGuesses}");
+            }
+            else if(guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (!found);
     }
 }
