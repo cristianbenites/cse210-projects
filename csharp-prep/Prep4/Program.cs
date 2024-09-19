2ad376fca8f4a4faeec7734c090457f6cbf2bc46
cristianbenites <cristian.jbenites@gmail.com>
2024-09-19 00:29:50 -0400

    finish prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        while(true)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if(number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+        }
+
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
