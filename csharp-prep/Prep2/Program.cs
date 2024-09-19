1215e089e090a4ac2eff9eb1e6ed404cd515560e
cristianbenites <cristian.jbenites@gmail.com>
2024-09-18 23:53:47 -0400

    finish prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+
+        string message = "Sorry! You didn't pass the class. But don't give up, you will get it next time!";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        if(grade >= 60)
+        {
+            if(grade % 10 >= 7)
+            {
+                if(grade < 90)
+                {
+                    letter += "+";
+                }
+            }
+            else
+            {
+                letter += "-";
+            }
+
+        }
+
+        if(grade >= 70)
+        {
+            message = "Congratulations on your approval! You passed the course.";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        Console.WriteLine(message);
     }
 }
