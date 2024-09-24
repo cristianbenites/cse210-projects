838956e7962f8653660c401cf83603fb3f31d6f5
cristianbenites <cristian.jbenites@gmail.com>
2024-09-23 22:05:26 -0400

    w02 - abstraction learning activity

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,17 @@
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public Resume()
+    {
+
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        _jobs.ForEach(job => job.Display());
+    }
+}
